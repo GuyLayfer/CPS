0c06c693515a425796fbf89d188296396fef0651
aviorbarkan <AviorB@gmail.com>
2017-11-26 13:56:36 +0200

    TestAvior

---
+++ src/client/Account.java
@@ -0,0 +1,22 @@
+package client;
+
+public class Account {
+	private double balance;
+	private String email;
+	
+	public double getBalance(){
+		return balance;
+	}
+	
+	public String getClientId(){
+		
+	}
+	
+	public String getClientLiscencePlate{
+		
+	}
+	
+	public String getClientEmail(){
+		
+	}
+}
