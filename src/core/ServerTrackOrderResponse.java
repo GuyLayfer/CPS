2f696d5191865e950eb84563247b682c2528b1ee
GuyLayfer <layfergp@gmail.com>
2017-12-21 14:04:49 +0200

    Prototype Server update

--- src/core/ServerTrackOrderResponse.java
+++ src/core/ServerTrackOrderResponse.java
@@ -2,13 +2,12 @@ package core;
 
 // The properties of this class are valid only if status == OK.
 public class ServerTrackOrderResponse extends ServerBasicResponse {
-	public String orderID;
-	public String customerID;
-	public String licensePlate;
-	public String email;
-	public String parkingLotID;
-	public String arrivalTime;
-	public String estimatedDepartureTime;
+	public int orderID;
+	public int customerID;
+	public String carID;
+	public int parkingLotID;
+	public long arrivalTime;				// milliseconds
+	public long estimatedDepartureTime;		// milliseconds
 	
 	public ServerTrackOrderResponse() {
 		super(ServerResponseStatus.OK, null);
