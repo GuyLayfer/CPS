22394294116bf67d44edc26e21bba4d973e74cce
Unknown <AviorB@gmail.com>
2018-01-05 21:16:47 +0200

    Host Address picker on Kiosk startup

---
+++ src/kioskGui/controllers/KioskOuterShellController.java
@@ -0,0 +1,11 @@
+package kioskGui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class KioskOuterShellController {
+
+	@FXML
+	private AnchorPane kisokOuterShellRegion;
+	
+}
