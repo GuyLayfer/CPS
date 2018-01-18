b559f07eb653d18e5dc02858181f4037ab129c80
hadeelmassarwa <33353799+hadeelmassarwa@users.noreply.github.com>
2018-01-18 14:46:06 +0200

    some more changes

--- src/server/workerServer/workerRequestsHandlers/DecideOnComplaintsRequestsHandler.java
+++ src/server/workerServer/workerRequestsHandlers/DecideOnComplaintsRequestsHandler.java
@@ -5,15 +5,11 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import core.worker.WorkerRequestType;
-import core.worker.WorkerRole;
 import core.worker.requests.BaseRequest;
 import core.worker.responses.WorkerResponse;
-import core.worker.responses.ComplaintsForReviewResponse;
+import core.worker.responses.WorkerBaseResponse;
 import core.worker.requests.DecideOnComplaintRequest;
-import core.worker.WorkerOperations;
 import server.workerServer.WorkerResponseFactory;
-import server.db.SqlColumns;
-import server.db.dbAPI.RegularDBAPI;
 
 public class DecideOnComplaintsRequestsHandler extends BaseRequestsHandler {
 
@@ -30,12 +26,7 @@ public class DecideOnComplaintsRequestsHandler extends BaseRequestsHandler {
 		if (resultList.isEmpty()) {
 			return createRequestDeniedResponse("Wrong ID or password");
 		}
-		Map<String, Object> result = resultList.iterator().next();
-		WorkerOperations workerOperations = WorkerOperations.valueOf((String)result.get(SqlColumns.Complaints.COMPLAINT_RESULT));
-		//ComplaintsForReviewResponse response = WorkerResponseFactory.CreateDecideOnComplaintsResponse();
- 
-		return createUnsupportedFeatureResponse();
-//		BaseResponse response = WorkerResponseFactory.CreateDecideOnComplaintsResponse();
-//		return CreateWorkerResponse(response);
+		WorkerBaseResponse response = WorkerResponseFactory.CreateDecideOnComplaintsResponse();
+		return CreateWorkerResponse(response);
 	}
 }
