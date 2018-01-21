b6a5ab8c605222912464ca29629bd6dec1194f74
Arthur <Foarthur@gmail.com>
2018-01-21 04:05:55 +0200

    trying to build query

--- src/server/requestHandlers/WebCustomerRequestsHandler.java
+++ src/server/requestHandlers/WebCustomerRequestsHandler.java
@@ -85,6 +85,10 @@ public class WebCustomerRequestsHandler extends AbstractServer {
 		if (resultList.isEmpty())
 			regularDBAPI.insertNewAccount(request.customerID, request.email, request.carID, TrueFalse.FALSE);
 		
+		//TODO: check for orders conflicts (if this car is already reserved in those times..)
+		//ArrayList<Map<String, Object>> resultList2 = new ArrayList<Map<String, Object>>();
+		//boolean hi = regularDBAPI.selectOrderByCarIdAndLotIdAndTime(request.carID, request.parkingLotID, request.arrivalTime,resultList2);
+		
 		int entranceID = regularDBAPI.insertParkingReservation(request.carID, request.customerID, request.parkingLotID,
 				request.arrivalTime, request.estimatedDepartureTime, new Date(0), new Date(0), 
 				OrderType.ORDER);
